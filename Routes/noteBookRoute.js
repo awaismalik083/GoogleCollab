2687ae7edbbd6d0dac1dd44577a43bc5f6b08970
Awais Malik <awaismalikfrom106@gmail.com>
2026-08-13 11:46:37 +0500

    Routes Fixed of Notebook Controller

--- Routes/noteBookRoute.js
+++ Routes/noteBookRoute.js
@@ -10,10 +10,9 @@ import {
 const noteBookRouter = Router();
 
 // All routes below require a valid logged-in user
-noteBookRouter.post("/create", authMiddleware, createNotebook); // POST   /api/notebooks
-// noteBookRouter.get("/", authMiddleware, getAllNotebooks);       // GET    /api/notebooks
-noteBookRouter.get("/getbyid:id", authMiddleware, getNotebookById); // GET    /api/notebooks/:id
-noteBookRouter.put("/update:id", authMiddleware, updateNotebook); // PUT    /api/notebooks/:id
-noteBookRouter.delete("/delete:id", authMiddleware, deleteNotebook); // DELETE /api/notebooks/:id
+noteBookRouter.post("/create", authMiddleware, createNotebook); // POST   /notebook/create
+noteBookRouter.get("/:id", authMiddleware, getNotebookById); // GET    /notebook/:id
+noteBookRouter.put("/:id", authMiddleware, updateNotebook); // PUT    /notebook/:id
+noteBookRouter.delete("/:id", authMiddleware, deleteNotebook); // DELETE /notebook/:id
 
 export default noteBookRouter;
